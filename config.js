64d0f352e3cb659d533de86d7a18ed517a23a0a7
Euler's Golden Pie <eulergoldpi@gmail.com>
2026-07-27 11:37:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,38 @@
+const CONFIG = {
+
+  // ==========================
+  // Google Apps Script
+  // ==========================
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxcmcBpycaXYQUf3Oj8_LXbBKFCSQqNf9gYMYGhp9siTnMbY6nJWpzi5bKJBlvrCdeG/exec",
+
+
+  // ==========================
+  // Test Settings
+  // ==========================
+  STARTING_SPAN: 2,
+  MAXIMUM_SPAN: 9,
+
+  CORRECT_TO_ADVANCE: 2,
+  ERRORS_TO_STOP: 2,
+
+  DIGIT_DISPLAY_TIME: 800, // milliseconds
+
+
+  // ==========================
+  // Participant Settings
+  // ==========================
+  REQUIRE_REGISTRATION_ID: true,
+
+  ALLOW_PRETEST: true,
+  ALLOW_POSTTEST: true,
+
+
+  // ==========================
+  // UI
+  // ==========================
+  APP_NAME: "EGP Digit Span Test",
+
+  VERSION: "1.0.0"
+
+};
 
